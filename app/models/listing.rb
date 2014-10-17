83f62e149c2c092b071c543bc067e36abab12f06
Cinnamon Bernard <cinnamon721@gmail.com>
2014-10-17 11:23:20 -0400

    Associate Listing with Users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -6,9 +6,13 @@ class Listing < ActiveRecord::Base
   					:storage => :dropbox,
   					:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
   					:path => ":style/:id_:filename"
-  	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+  	
   end
   validates :name, :description, :price, presence: true
   validates :price, numericality: { greater_than: 0 }
-  validates_attachment_presence :image
+#  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_presence :image, :content_type => /\Aimage\/.*\Z/  
+
+  belongs_to :user
 end
